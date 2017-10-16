755c86d0fa8c5cb6deadb3c42481e7b0c266ff01
Nana Kerber <nanakerber@gmail.com>
2017-10-16 22:40:38 +0300

    Initial commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,12 +1,12 @@
 public class Fibonacci {
     public static void main(String[] args) {
-            System.out.println(fib(30));
+        System.out.println(fib(30));
     }
 
     private static int fib(int n) {
         if (n == 0) {
             return 0;
-       }else if (n == 1) {
+        } else if (n == 1) {
             return 1;
         } else {
             return fib(n - 1) + fib(n - 2);
