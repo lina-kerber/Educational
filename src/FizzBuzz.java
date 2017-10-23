2401dc5d62da05f9985b38d30b87b43732f8926a
Nana Kerber <nanakerber@gmail.com>
2017-10-23 22:17:15 +0300

    Remove old FizBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,19 +1,19 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-        System.out.println(fb(25));
+    public static void main (String[] args) {
+        fb(30);
     }
 
-    private static int fb(int n) {
-        for (int i = 0; i <= n; i++) {
+    private static void fb(int n) {
+        for (int i = 1; i <= n; i++) {
             if (i % 5 == 0 && i % 3 == 0) {
-                System.out.println("FizzBuzz = " + i);
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.println("Fizz = " + i);
+                System.out.println("Fizz");
             } else if (i % 5 == 0) {
-                System.out.println("Buzz = " + i);
+                System.out.println("Buzz");
             } else {
-                System.out.println("not = " + i);
+                System.out.println(i);
             }
-        } return n;
+        }
     }
 }
