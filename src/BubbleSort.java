4b71653de3dc7207a9968c240704e7686b4abdeb
Nana Kerber <nanakerber@gmail.com>
2017-11-07 00:52:25 +0300

    Bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 8, 6};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    private static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
