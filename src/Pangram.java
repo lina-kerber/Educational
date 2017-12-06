80423b85e2b9c02c91d3215959e0ef3104114a1d
Nana Kerber <nanakerber@gmail.com>
2017-12-06 23:54:36 +0300

    metod isPangram String "The quick brown fox jumps over the lazy dog"

--- src/Pangram.java
+++ src/Pangram.java
@@ -2,21 +2,17 @@ import java.util.HashSet;
 
 public class Pangram {
     public static void main(String[] args) {
-        String str = "The quick brown fox jumps over the lazy dog";
-        System.out.println(letter(str));
+        String str = "The quick brown fox jumps over the lazy dog.";
+        System.out.println(isPangram(str));
     }
 
-    private static boolean letter(String str) {
-        boolean result = (true);
+    private static boolean isPangram(String str) {
+        boolean result = true;
         HashSet<Character> ch = new HashSet<>();
         for (char c : str.toLowerCase().toCharArray()) {
-            if (!ch.contains(c) && Character.isLetter(c) || Character.isWhitespace(c)) {
+            if (!ch.contains(c) && Character.isLetter(c)) {
                 ch.add(c);
-                result = ch.size() == 27;
-
-            } else if (!ch.contains(c) && !Character.isLetter(c)) {
-                result = false;
-                break;
+                result = ch.size() == 26;
             }
         }
         return result;
